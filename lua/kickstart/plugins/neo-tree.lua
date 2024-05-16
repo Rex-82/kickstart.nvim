83d4293489afe54425318a41d9cd4472577af068
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-05-16 16:40:47 +0200

    feat: add neotree buffers keybind

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,6 +12,7 @@ return {
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    { '|', ':Neotree buffers<CR>', { desc = 'NeoTree buffers' } },
   },
   opts = {
     filesystem = {
