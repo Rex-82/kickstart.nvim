611ee370f8787102d43af54eddbc7e90584c3bc5
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-07-02 15:33:39 +0200

    feat: add alpha home

--- lua/custom/plugins/highlightcolors.lua
+++ lua/custom/plugins/highlightcolors.lua
@@ -3,26 +3,29 @@ return {
 
   config = function()
     require('nvim-highlight-colors').setup {
-      ---Render style
-      ---@usage 'background'|'foreground'|'virtual'
+      -- Render style
+      -- @usage 'background'|'foreground'|'virtual'
       render = 'virtual',
 
-      ---Set virtual symbol (requires render to be set to 'virtual')
+      -- Set virtual symbol (requires render to be set to 'virtual')
       virtual_symbol = '●',
 
-      ---Highlight named colors, e.g. 'green'
+      -- Highlight named colors, e.g. 'green'
       enable_named_colors = true,
 
-      ---Highlight tailwind colors, e.g. 'bg-blue-500'
-      enable_tailwind = false,
+      -- Highlight tailwind colors, e.g. 'bg-blue-500'
+      enable_tailwind = true,
 
-      ---Set custom colors
-      ---Label must be properly escaped with '%' to adhere to `string.gmatch`
-      --- :help string.gmatch
+      -- Set custom colors
+      -- Label must be properly escaped with '%' to adhere to `string.gmatch`
+      --  :help string.gmatch
       custom_colors = {
         { label = '%-%-theme%-primary%-color', color = '#0f1219' },
         { label = '%-%-theme%-secondary%-color', color = '#5a5d64' },
       },
+      -- Exclude filetypes or buftypes from highlighting e.g. 'exclude_buftypes = {'text'}'
+      exclude_filetypes = {},
+      exclude_buftypes = {},
     }
   end,
 }
