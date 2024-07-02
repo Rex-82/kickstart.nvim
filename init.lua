04c450e4069c2839f2631aa26ee114ab3bafc2bb
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-07-02 15:30:14 +0200

    feat: add emmet lsp

--- init.lua
+++ init.lua
@@ -600,6 +600,43 @@ require('lazy').setup({
             },
           },
         },
+        emmet_language_server = {
+          filetypes = { 'eruby', 'html', 'javascript', 'javascriptreact', 'pug', 'typescriptreact', 'php' },
+          -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+          -- **Note:** only the options listed in the table are supported.
+          init_options = {
+            ---@type table<string, string>
+            includeLanguages = {},
+            --- @type string[]
+            excludeLanguages = {},
+            --- @type string[]
+            extensionsPath = {},
+            --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+            preferences = {},
+            --- @type boolean Defaults to `true`
+            showAbbreviationSuggestions = true,
+            --- @type "always" | "never" Defaults to `"always"`
+            showExpandedAbbreviation = 'always',
+            --- @type boolean Defaults to `false`
+            showSuggestionsAsSnippets = false,
+            --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+            syntaxProfiles = {},
+            --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+            variables = {},
+          },
+        },
+        -- stylelint_lsp = {
+        --   filetypes = { 'css', 'scss' },
+        --   root_dir = require('lspconfig').util.root_pattern('package.json', '.git'),
+        --   settings = {
+        --     stylelintplus = {
+        --       -- see available options in stylelint-lsp documentation
+        --     },
+        --   },
+        --   on_attach = function(client)
+        --     client.server_capabilities.document_formatting = false
+        --   end,
+        -- },
       }
 
       -- Ensure the servers and tools above are installed
