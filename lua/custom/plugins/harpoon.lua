b4cee10db780e873b86680cdb618c0618dfe19cf
Simone Ferretti <simone.ferretti.work@gmail.com>
2025-03-15 15:31:58 +0100

    chore(harpoon): use o as harpoon action prefix

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -9,26 +9,26 @@ return {
     harpoon:setup()
     -- REQUIRED
 
-    vim.keymap.set('n', '<leader>ha', function()
+    vim.keymap.set('n', '<leader>oa', function()
       harpoon:list():add()
     end, { desc = 'Add to Harpoon' })
-    vim.keymap.set('n', '<leader>hr', function()
+    vim.keymap.set('n', '<leader>or', function()
       harpoon:list():remove()
     end, { desc = 'Remove from Harpoon' })
     -- vim.keymap.set('n', '<leader>he', function()
     --   harpoon.ui:toggle_quick_menu(harpoon:list())
     -- end, { desc = 'Open harpoon list' })
 
-    vim.keymap.set('n', '<leader>h1', function()
+    vim.keymap.set('n', '<leader>1', function()
       harpoon:list():select(1)
     end, { desc = 'Go to Harpoon 1' })
-    vim.keymap.set('n', '<leader>h2', function()
+    vim.keymap.set('n', '<leader>2', function()
       harpoon:list():select(2)
     end, { desc = 'Go to Harpoon 2' })
-    vim.keymap.set('n', '<leader>h3', function()
+    vim.keymap.set('n', '<leader>3', function()
       harpoon:list():select(3)
     end, { desc = 'Go to Harpoon 3' })
-    vim.keymap.set('n', '<leader>h4', function()
+    vim.keymap.set('n', '<leader>4', function()
       harpoon:list():select(4)
     end, { desc = 'Go to Harpoon 4' })
 
@@ -43,6 +43,17 @@ return {
     -- basic telescope configuration
     local conf = require('telescope.config').values
     local function toggle_telescope(harpoon_files)
+      local finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(paths, item.value)
+        end
+
+        return require('telescope.finders').new_table {
+          results = paths,
+        }
+      end
+
       local file_paths = {}
       for _, item in ipairs(harpoon_files.items) do
         table.insert(file_paths, item.value)
@@ -56,11 +67,24 @@ return {
           },
           previewer = conf.file_previewer {},
           sorter = conf.generic_sorter {},
+          attach_mappings = function(prompt_bufnr, map)
+            -- TODO: add label to command (how?)
+
+            map('i', '<C-d>', function()
+              local state = require 'telescope.actions.state'
+              local selected_entry = state.get_selected_entry()
+              local current_picker = state.get_current_picker(prompt_bufnr)
+
+              table.remove(harpoon_files.items, selected_entry.index)
+              current_picker:refresh(finder())
+            end)
+            return true
+          end,
         })
         :find()
     end
 
-    vim.keymap.set('n', '<leader>he', function()
+    vim.keymap.set('n', '<leader>oe', function()
       toggle_telescope(harpoon:list())
     end, { desc = 'Open harpoon window' })
   end,
